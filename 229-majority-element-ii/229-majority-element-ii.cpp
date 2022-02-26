c4b7a4cc6d318c5b60d1e3b0c545f1acd7a8dc07
ke511081177 <ke511081177@gmail.com>
2022-02-26 23:15:36 +0800

    Time: 12 ms (86.80%), Space: 15.7 MB (83.04%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
+        // Boyer–Moore majority vote algorithm
+        // This is two-pass algo.
+        // pass1:  Evey voter can vote to one candidate. And choose two winner
+        // pass2:  Evey voter vote to those two candiate or vote null. And check if those pass 1/3 threshold or not.
+        // Time/Space complexity: O(N), O(1)
+        // Have a good day.
+        
         vector<int> res;
         int len = nums.size();
         int m1 = -1, m2 = -1;
         int vote1 = 0, vote2 = 0;
         
-        for(auto c:nums){
+        for(auto &c:nums){                //pass1
             if(m1 == c) vote1++;
             else if(m2 == c) vote2++;
             
@@ -28,7 +35,7 @@ public:
         vote1 = 0;
         vote2 = 0;
         
-        for(auto c:nums){
+        for(auto &c:nums){                         //pass1
             if(m1 == c) vote1++;
             else if(m2 == c) vote2++;
         }
@@ -37,7 +44,5 @@ public:
         if(vote2 > (len/3)) res.push_back(m2);
         
         return res;
-        
-        
     }
 };
