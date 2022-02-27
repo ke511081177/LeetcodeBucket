bb3113d051f6e39cd30dd438ccf3d0e7ec66af04
ke511081177 <ke511081177@gmail.com>
2022-02-27 19:47:44 +0800

    Time: 3 ms (77.70%), Space: 6.7 MB (24.21%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -3,13 +3,14 @@ public:
     char findTheDifference(string s, string t) {
         
         char res;
-        int lens = s.size(), lent = t.size();
+        int lens = s.size();
         
-        for(int i = 0; i < lens; i++){
+        for(int i = 0; i < lens; i++)
             res += t[i]-s[i];
             
-        }
+        
         res += t[lens];
+        
         return res;
     }
 };
