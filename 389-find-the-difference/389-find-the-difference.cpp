255932cc0da9dac324ff81b71942c1311b1bd708
ke511081177 <ke511081177@gmail.com>
2022-02-27 19:47:02 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (66.58%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        char res;
+        int lens = s.size(), lent = t.size();
+        
+        for(int i = 0; i < lens; i++){
+            res += t[i]-s[i];
+            
+        }
+        res += t[lens];
+        return res;
+    }
+};
