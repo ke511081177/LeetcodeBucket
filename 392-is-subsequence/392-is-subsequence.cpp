e945db708c1d0cde52a12c6166682653e1bdffbf
ke511081177 <ke511081177@gmail.com>
2022-03-02 14:09:48 +0800

    Time: 2 ms (52.38%), Space: 6.4 MB (33.59%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -5,13 +5,13 @@ public:
         if(s.size() == 0) return true;
         if(t.size() == 0) return false;
         
-        int i = 0, len = s.size();
+        int i = 0;
         
         for(auto c:t){
             if(s[i] == c)
                 i++;
            
-            if(i == len)
+            if(i ==  s.size())
                 return true;
         }
         return false;
