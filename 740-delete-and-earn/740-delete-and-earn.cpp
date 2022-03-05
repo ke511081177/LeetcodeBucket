46f5b04007ecba754300c130c3733a53eb37758e
ke511081177 <ke511081177@gmail.com>
2022-03-05 15:19:43 +0800

    Time: 21 ms (20.76%), Space: 13.9 MB (21.50%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        
+        int len = 10001;
+        vector<int> sum(len, 0);
+        vector<int> dp(len, 0);
+
+        for(auto num: nums){
+            sum[num] += num;
+        }
+   
+        
+        dp[0] = 0;
+        dp[1] = sum[1];
+        //now apply the house robbing concept
+        for(int i=2; i<len; i++){
+            dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
+        }
+
+        return dp[len-1];
+        
+    }
+};
