b4fef6f2e7df3874a24deb6c853dcfa02a2fc2bb
ke511081177 <ke511081177@gmail.com>
2022-03-12 15:27:11 +0800

    Time: 8 ms (74.56%), Space: 6.7 MB (57.65%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        if(n == 1) return "1";
+        if(n == 2) return "11";
+        
+        string res = "11";
+        
+        n-= 2;
+        
+        while(n-- > 0){
+            
+            string temp = "";
+            int len = res.size();
+            int count = 1;
+            char digit = res[0];
+            
+            for(int i = 1; i < len; i++){
+                if(res[i] != digit){
+                    temp += to_string(count);
+                    temp.push_back(digit);
+            
+                    digit = res[i];
+                    count = 1;
+                }
+                else
+                    count ++;
+            }
+            
+            temp += to_string(count);
+            temp.push_back(digit);
+
+            res = temp;
+        }
+        return res;
+    }
+};
