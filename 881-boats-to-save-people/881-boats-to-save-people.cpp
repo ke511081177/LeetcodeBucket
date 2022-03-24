6d935b8fe1a98ab8e415e2e99c77810c504f1a7c
ke511081177 <ke511081177@gmail.com>
2022-03-24 21:57:08 +0800

    Time: 162 ms (11.88%), Space: 42 MB (10.43%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        sort(people.begin(),people.end());
+        int res = 0;
+        int i = 0,j = people.size()-1;
+        
+        while(i <= j){
+            if(people[i]+people[j] <= limit){
+                i++;
+                j--;
+            }
+            else
+                j--;
+            res++;
+        }
+        return res;
+    }
+};
