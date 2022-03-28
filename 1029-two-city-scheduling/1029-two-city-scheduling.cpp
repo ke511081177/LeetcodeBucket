3129b2571a5bf5242b99ee321e127c5b913dcd11
ke511081177 <ke511081177@gmail.com>
2022-03-28 15:11:23 +0800

    Time: 11 ms (23.11%), Space: 7.9 MB (43.02%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -8,14 +8,14 @@ public:
         vector<int> diff(len,0);
         
         for(int i = 0; i < len; i++){
-            toA+= costs[i][0];
-            diff[i] = costs[i][1] - costs[i][0];
+            toA+= costs[i][0];                         // cost of all to 
+            diff[i] = costs[i][1] - costs[i][0];       // diff of A and B
         }
         
         sort(diff.begin(),diff.end());
         
-        for(int i = 0;i < len/2;i++){
-            toA += diff[i];
+        for(int i = 0;i < len/2;i++){               
+            toA += diff[i];                            // half to B, and add diff of to A and to B
             
         }
         return toA;
