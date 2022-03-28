b64a6cde56d7020ac7000f82424c4f5a3f95ca91
ke511081177 <ke511081177@gmail.com>
2022-03-28 15:01:56 +0800

    Time: 45 ms (45.67%), Space: 25.8 MB (9.85%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        vector<int> temp;
+        int len = nums.size();
+        k %= len;
+        if( k == 0 || len/k == 0)return ;
+        
+        for(int i = k-1; i >= 0; i--){
+            temp.push_back(nums[len-i-1]);
+            // temp[i] = nums[len-i-1];
+        }
+        for(int i = len-k-1; i >= 0; i-- )
+            nums[i+k] = nums[i];
+        
+        for(int i = 0; i < k; i++)
+            nums[i] = temp[i];
+        
+    }
+};
