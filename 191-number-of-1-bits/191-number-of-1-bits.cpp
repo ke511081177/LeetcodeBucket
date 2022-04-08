e2e00364b7dcb3f52288ebf3a97f271c75751674
ke511081177 <ke511081177@gmail.com>
2022-04-08 11:57:09 +0800

    Time: 4 ms (37.49%), Space: 6 MB (48.19%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int res = 0;
+
+        while(n != 0){
+            res++; 
+            n &= n-1;
+            
+        }
+        return res;
+    }
+};
